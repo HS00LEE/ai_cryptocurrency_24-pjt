432aab52b5b0003cecf003a20d81bbc1167d8d67
hslee <hyeshin.lee@lge.com>
2024-04-29 09:12:12 +0900

    orderbook pjt finished

--- orderBookTest.py
+++ orderBookTest.py
@@ -4,49 +4,72 @@ import datetime
 import requests
 import pandas as pd
 
-SLEEPDELAY  = 5
+SLEEPDELAY  = 5 #seconds
 
-testcount = 0
-while(testcount < 2):
-    testcount += 1
-    #response = requests.get ('https://api.bithumb.com/public/orderbook/BTC_KRW/?count=10')
-    #print (response.text)
 
+def main():
+    testcount = 0
+    testInfinite = True
+
+    while(testcount < 18000):
+        testcount += 1
+
+        recordOderbook('BTC')
+        time.sleep(1)
+        recordOderbook('ETH')
+
+        time.sleep(SLEEPDELAY-1)
+
+
+def fetchOrderbook(targetSymbol):
     book = {}
-    response = requests.get ('https://api.bithumb.com/public/orderbook/BTC_KRW/?count=5')
-    book = response.json()
-    #print (response.status_code)
+    response = requests.get ('https://api.bithumb.com/public/orderbook/' + targetSymbol + '/?count=5')
+    if response.status_code == 200:
+        book = response.json()
+        print(targetSymbol + ' get result: ' + str(response.status_code))
+        return book
+    else:
+        return ""
 
-    data = book['data']
 
-    print (data)
+def recordOderbook(coinName):
+    symbolName = coinName + '_KRW'
+    bookResult = fetchOrderbook(symbolName)
+    if bookResult == "":
+        print('Data get fail')
+        return
+    #print (bookResult)
+    data = bookResult['data']
+
+    #print (data)
 
     epochTimestamp = data['timestamp']  #milliseconds
     convertedTimestamp = datetime.datetime.fromtimestamp(float(epochTimestamp)/1000.0).strftime('%Y-%m-%d %H:%M:%S.%f')
-    
+
+    #bid(buy) order book
     bids = (pd.DataFrame(data['bids'])).apply(pd.to_numeric,errors='ignore')
     bids.sort_values('price', ascending=False, inplace=True)
     bids = bids.reset_index(); del bids['index']
     bids['type'] = 0
     bids['timestamp'] = convertedTimestamp
-    
-    
+
+    #ask(sell) order book
     asks = (pd.DataFrame(data['asks'])).apply(pd.to_numeric,errors='ignore')
     asks.sort_values('price', ascending=True, inplace=True)
     asks['type'] = 1
     asks['timestamp'] = convertedTimestamp
-    
-    # Pandas lib issue. change to concat
+
+    # Pandas lib version changes.use concat
     # df = bids.append(asks)
     df = pd.concat([bids, asks])
-
-    print (df)
+    #print (df)
 
     # Remove data frame index. If file exists skip header
-    filename = convertedTimestamp[:10] + "-bithumb-BTC-orderbook.csv"
+    filename = convertedTimestamp[:10] + '-bithumb-' + coinName + '-orderbook.csv'
     if not os.path.isfile(filename):
         df.to_csv(filename, index=False)
     else:
         df.to_csv(filename, index=False, mode='a', header=False)
-    
-    time.sleep(SLEEPDELAY)
+
+
+main()
